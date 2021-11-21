b9ab6cf470f46390dad9c141c76d1db92255de42
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2021-11-21 12:06:28 -0800

    added black to files for testing

--- mysite/blogging/tests.py
+++ mysite/blogging/tests.py
@@ -55,6 +55,7 @@ class FrontEndTestCase(TestCase):
                 self.assertContains(resp, title, count=1)
             else:
                 self.assertNotContains(resp, title)
+                
     def test_details_only_published(self):
         for count in range(1, 11):
             title = "Post %d Title" % count
