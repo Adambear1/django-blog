f92d2eb7c696e68beca510c4934138f8293b0c98
Adambear1 <adamcarlbirgenheier@gmail.com>
2022-01-08 07:29:40 -0600

    final updates.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,5 @@
 """mysite URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
@@ -15,12 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+from blogging import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'api/users', views.UserViewSet)
+router.register(r'api/groups', views.GroupViewSet)
+router.register(r'api/posts', views.PostViewSet)
+router.register(r'api/categories', views.CategoryViewSet)
 
 urlpatterns = [
     path('', include('blogging.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
-
+    path('accounts/', include('allauth.urls')),
 ]
